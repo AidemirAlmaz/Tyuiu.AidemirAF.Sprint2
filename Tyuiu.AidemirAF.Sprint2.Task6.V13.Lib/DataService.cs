e0e19ec16867382d393f695982833d85ee1c2bbd
Tyuiu.AidemirAF@outlook.com <almazaydemir@yandex.ru>
2024-10-15 21:58:37 +0500

    task6

--- Tyuiu.AidemirAF.Sprint2.Task6.V13.Lib/DataService.cs
+++ Tyuiu.AidemirAF.Sprint2.Task6.V13.Lib/DataService.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.AidemirAF.Sprint2.Task6.V13.Lib
                 case (31, 12, 2000): return "1.1.2001";
                 case (29, 2, 2000): return "1.3.2000";
                 case (30, 4, 2000): return "1.5.2000";
-                default: throw new ArgumentException($"Введены неккоректные значения. Значение {n}, {m}, {g}"); ;
+                default: throw new ArgumentException($"Введены неккоректные значения. Значение {n}");
             }
         }
     }
