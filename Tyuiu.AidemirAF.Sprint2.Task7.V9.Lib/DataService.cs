55c2105d5f991425a2c50a1b433d1aaa68be97d4
Tyuiu.AidemirAF@outlook.com <almazaydemir@yandex.ru>
2024-10-16 22:25:15 +0500

    Task7

---
+++ Tyuiu.AidemirAF.Sprint2.Task7.V9.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.AidemirAF.Sprint2.Task7.V9.Lib
+{
+    public class DataService : ISprint2Task7V9
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+            if (((y <= 0.5) && (y >= 0)) && (y <= Math.Sin(x)))
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.AidemirAF.Sprint2.Task7.V9/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.AidemirAF.Sprint2.Task7.V9.Lib;
+
+/*
+ * ЗАДАНИЕ
+ * Написать программу на С#, которая запрашивает исходные данные (вещественные значения) и вычисялет,
+ * находится ли точка с координатами X, Y в заштрихованной области.
+ */
+
+namespace Tyuiu.AidemirAF.Sprint2.Task7.V9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("*****************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                  *");
+            Console.WriteLine("*****************************************************************************************************");
+
+            Console.WriteLine("Введите значение переменной X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine("*****************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                        *");
+            Console.WriteLine("*****************************************************************************************************");
+
+            if (res)
+            {
+                Console.WriteLine("Точка находится в заштрихованной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находится в заштрихованной области");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
